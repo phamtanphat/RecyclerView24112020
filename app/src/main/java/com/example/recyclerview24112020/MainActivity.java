6d55595531611cb026ad6ebb3c290f4110941d8e
phatdroid94 <phatdroid94@gmail.com>
2020-12-22 19:19:25 +0700

    List interface

--- app/src/main/java/com/example/recyclerview24112020/MainActivity.java
+++ app/src/main/java/com/example/recyclerview24112020/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Array : Dang mang Array
-        // Cach 1: Khoi tao va gan gia tri
+//        Array : Dang mang Array
+//        Cach 1: Khoi tao va gan gia tri
 //                            0 , 1 ,2
 //        String[] arrNames = {"Teo","Ti","Tun"};
 //        arrNames[0] = "Tien";
@@ -23,8 +26,19 @@ public class MainActivity extends AppCompatActivity {
 //        Cach 2 : Khoi tao kich thuoc
 //        String[] arrNames2 = new String[2];
 //        Log.d("BBB",arrNames2[0] +"");
+
         // Collection : Dang mang theo nhom cau truc collection
-
-
+        List<String> strings = new ArrayList<>();
+        // Them du lieu
+        strings.add("Teo");
+        strings.add("Ti");
+        // Cap nhat
+        strings.set(0,"Tien");
+        // Xoa
+        strings.remove(0);
+        // Lay gia phan tu
+        Log.d("BBB",strings.get(0));
+        // Lay kich thuoc mang
+        Log.d("BBB",strings.size() + "");
     }
 }
